5f149eefbec2e332eeb883d1f5720d04a1e29312
naoki-yoshimoto1114 <n.yoshimoto1114@gmail.com>
2023-07-13 23:18:31 +0900

    first commit

---
+++ src/test/java/com/example/CookingGenerator/CookingGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CookingGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
